3a6265e6164043cbc6be5531c2486723b391175c
iso-lib <77186879+iso-lib@users.noreply.github.com>
2021-03-29 10:01:35 +0800

    Add files via upload

---
+++ baidu_tts.js
@@ -0,0 +1,57 @@
+const AipSpeech = require("baidu-aip-sdk").speech;
+const fs = require('fs');
+
+module.exports = function (RED) {
+
+	function baiduTTSNode(config) {
+        RED.nodes.createNode(this, config);
+
+        // Retrieve the config node
+        this.server = RED.nodes.getNode(config.server);
+        var node = this;
+        if (this.server) {
+
+        } else {
+            node.error("没有配置正确的baidu-tts server");
+            return
+        }
+		let baidu_apiKey = this.server.apiKey;
+		let baidu_secretKey = this.server.secretKey;
+		
+		let client = new AipSpeech(0, baidu_apiKey, baidu_secretKey);
+
+		//let voice = fs.readFileSync('../assets/16k_test.pcm');
+		//let voice = fs.readFileSync("assets");
+		//let voiceBase64 = new Buffer(voice);
+		
+		node.on('input', function (msg) {
+			var payload = {};
+			var data = msg.data;
+			// 语音合成，保存到本地文件
+			client.text2audio(data, {spd: 0, per: 4}).then(function(result) {
+				if (result.data) {
+					fs.writeFileSync('tts.mp3', result.data);
+					node.log('语音合成成功，文件保存到tts.mp3，打开听听吧');
+				} else {
+					// 合成服务发生错误
+				//	console.log('语音合成失败: ' + JSON.stringify(result));
+				node.log('语音合成失败: ' + JSON.stringify(result));
+				}
+			}, function(err) {
+				return
+			})
+		});
+		
+	}
+	RED.nodes.registerType("baidu-tts", baiduTTSNode);
+	
+	
+	function RemoteServerNode(baidu) {
+        RED.nodes.createNode(this, baidu);
+        this.name = baidu.name;
+        this.apiKey = baidu.apiKey;
+        this.secretKey= baidu.secretKey;
+	}
+	RED.nodes.registerType("baidu-tts-server", RemoteServerNode);
+
+}
