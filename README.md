760e2941d9f6a0dd52ad690066ca5844fe7fc217
iso-lib <77186879+iso-lib@users.noreply.github.com>
2021-03-29 23:15:23 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,11 +1,42 @@
-# 语音合成，语音识别 RESTFul API nodejs sdk 使用示例代码
 
-## 准备
-1. 首先拷贝整个speech目录内容，务必包含assets目录下的文件
-2. 进入node文件夹，运行 ```npm install``` 安装包依赖
-3. 在main.js文件里填写您在百度云控制台新建应用的Api Key和Secret Key
-4. 运行 ```node main.js``` 运行代码
+# 欢迎使用node-red-contrib-baidu-tts
 
-## 说明
-1. 语音合成调用如果后会保存二进制内容到tts.mp3文件，可直接播放。
-2. 直接运行报错，因为main.js中没有设置Api Key和Secret Key，请先在百度云控制台新建语音服务应用获取后填入。
+
+
+
+## 本程序是node-red的节点模块,基于百度开放API进行开发调试
+
+## 节点功能为将输入的文字转为语音,并存储为mp3文件
+#### 可直接在本节点TTS文本框内输入文字,如果留空则会使用上个节点传来的msg.data的内容
+
+要使用本节点,请登录[百度AI开放平台](https://ai.baidu.com/tech/speech)，创建语音应用获取参数
+#### 需要3个参数
+1. App ID
+2. Api Key
+3. Secret Key
+
+
+#### 额外参数选填
+1. 发音角色
+2. 语速
+3. 语调
+4. 音量
+5. mp3保存路径
+
+
+
+## 安装
+npm i node-red-contrib-baidu-tts
+
+## 使用
+
+![配置](https://ae01.alicdn.com/kf/Ud1dce69cda1f4de0a9436eae5f61a525u.jpg)
+
+![服务器配置](https://ae01.alicdn.com/kf/U4811bedf8ec14f5eaffcde34765454e9R.jpg)
+
+![上个节点传入](https://ae01.alicdn.com/kf/U134be02b57b54e2d8602ee175b94abbdQ.jpg)
+
+
+结束
+****
+    
--- baidu_tts.js
+++ baidu_tts.js
@@ -15,30 +15,38 @@ module.exports = function (RED) {
             node.error("没有配置正确的baidu-tts server");
             return
         }
+		let baidu_appID = this.server.appID;
 		let baidu_apiKey = this.server.apiKey;
 		let baidu_secretKey = this.server.secretKey;
-		
-		let client = new AipSpeech(16193101, baidu_apiKey, baidu_secretKey);
+		let client = new AipSpeech(baidu_appID, baidu_apiKey, baidu_secretKey);
 
-		//let voice = fs.readFileSync('../assets/16k_test.pcm');
-		//let voice = fs.readFileSync("assets");
-		//let voiceBase64 = new Buffer(voice);
-		
 		node.on('input', function (msg) {
 			var payload = {};
-			var data = msg.data;
+			//var data = msg.data;
+			var data;
+			if (config.data != '' && config.data != null ) {
+				data = config.data;
+			} else {
+				data = msg.data;
+			}
+			if (config.path != '' && config.path != null ) {
+				path = config.path;
+			} else {
+				path = 'tts.mp3'
+			}
+			var options = {spd: config.spd, pit: config.pit, vol: config.vol, per: config.per};
 			// 语音合成，保存到本地文件
-			client.text2audio(data, {spd: 5, per: 0}).then(function(result) {
+			client.text2audio(data, options).then(function(result) {
 				if (result.data) {
-					fs.writeFileSync('tts.mp3', result.data);
-					node.log('语音合成成功，文件保存到tts.mp3，打开听听吧');
+					fs.writeFileSync(path, result.data);
+					node.status({ text: `语音合成成功，保存为 ${path}` });
 				} else {
-					// 合成服务发生错误
-				//	console.log('语音合成失败: ' + JSON.stringify(result));
-				node.log('语音合成失败: ' + JSON.stringify(result));
+				// 合成服务发生错误
+				node.status({ text: '语音合成失败,请检查配置' });
+				return
 				}
 			}, function(err) {
-				node.log(err);
+				node.warn(err);
 			})
 		});
 		
@@ -49,6 +57,7 @@ module.exports = function (RED) {
 	function RemoteServerNode(baidu) {
         RED.nodes.createNode(this, baidu);
         this.name = baidu.name;
+		this.appID = baidu.appID;
         this.apiKey = baidu.apiKey;
         this.secretKey= baidu.secretKey;
 	}
