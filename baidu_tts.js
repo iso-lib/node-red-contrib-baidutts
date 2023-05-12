593d638f42f4cc0e3529c2529b791f98c99e4a38
iso-lib <77186879+iso-lib@users.noreply.github.com>
2023-05-12 12:09:22 +0800

    1.0.14

--- baidu_tts.js
+++ baidu_tts.js
@@ -1,11 +1,12 @@
 const AipSpeech = require("baidu-aip-sdk").speech;
-const request = require('request');
 const fs = require('fs');
 
 module.exports = function (RED) {
+
 	function baiduTTSNode(config) {
         RED.nodes.createNode(this, config);
 
+        // Retrieve the config node
         this.server = RED.nodes.getNode(config.server);
         var node = this;
         if (this.server) {
@@ -18,70 +19,35 @@ module.exports = function (RED) {
 		let baidu_apiKey = this.server.apiKey;
 		let baidu_secretKey = this.server.secretKey;
 		let client = new AipSpeech(baidu_appID, baidu_apiKey, baidu_secretKey);
-		let baidu_platform = this.server.platform;
+
 		node.on('input', function (msg) {
 			var payload = {};
-			var ttsData,ttsPath;
+			//var data = msg.data;
+			var data;
 			if (config.data != '' && config.data != null ) {
-				ttsData = config.data;
+				data = config.data;
 			} else {
-				ttsData = msg.data;
+				data = msg.data;
 			}
 			if (config.path != '' && config.path != null ) {
-				ttsPath = config.path;
+				path = config.path;
 			} else {
-				ttsPath = 'tts.mp3'
+				path = 'tts.mp3'
 			}
-			switch(baidu_platform)
-			{
-				case "baidu-ai":
-					var options = {spd: config.spd, pit: config.pit, vol: config.vol, per: config.per, aue: config.aue};
-					client.text2audio(ttsData, options).then(function(result) {
-						if (result.data) {
-							fs.writeFileSync(ttsPath, result.data);
-							node.status({ text: `百度智能语音，保存为 ${ttsPath}` });
-							node.send({platform: 'baidu-ai' , result});
-						} else {
-							node.status({ text: '语音合成失败,请检查配置' });
-							node.send({platform: 'baidu-ai' , result});
-						}
-					}, function(err) {
-						node.warn(err);
-					})
-					break;
-				case "baidu-fanyi":
-					var ttsUrl = "https://fanyi.baidu.com/gettts";
-					var writeStream=fs.createWriteStream(ttsPath , {autoClose : true})
-					request({
-						url : ttsUrl, 
-						method: 'POST',
-						headers : {
-							'content-type' : "application/x-www-form-urlencoded"
-							},
-						form :{
-							text : ttsData,
-							spd : 5,
-							lan : "zh",
-							source : "web"
-						}}, function (error ,response, body) {
-							if (!error && response.statusCode == 200) {
-								node.status({ text: `百度翻译语音，保存为 ${ttsPath}` });
-							} else {
-								node.status({ text: "语音合成失败,请检查配置" });
-								node.send({platform: 'baidu-fanyi' , result: error});
-							}
-						}).pipe(writeStream);
-						writeStream.on('finish',function(){
-							fs.readFile(ttsPath , function(err,body) {
-								if (err) throw err;
-								var buffer = {data:body};
-								node.send({platform: 'baidu-fanyi' , result: buffer});
-							});
-						});
-					break;
-				default:
-					node.send({ platform: "请在TTS服务配置里选择语音合成平台" });
-			}
+			var options = {spd: config.spd, pit: config.pit, vol: config.vol, per: config.per};
+			// 语音合成，保存到本地文件
+			client.text2audio(data, options).then(function(result) {
+				if (result.data) {
+					fs.writeFileSync(path, result.data);
+					node.status({ text: `语音合成成功，保存为 ${path}` });
+				} else {
+				// 合成服务发生错误
+				node.status({ text: '语音合成失败,请检查配置' });
+				}
+				node.send(result)
+			}, function(err) {
+				node.warn(err);
+			})
 		});
 		
 	}
@@ -91,7 +57,6 @@ module.exports = function (RED) {
 	function RemoteServerNode(baidu) {
         RED.nodes.createNode(this, baidu);
         this.name = baidu.name;
-		this.platform = baidu.platform;
 		this.appID = baidu.appID;
         this.apiKey = baidu.apiKey;
         this.secretKey= baidu.secretKey;
